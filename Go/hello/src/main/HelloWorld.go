af7a0d1f3f28c3f8163de99398df5b9381c90f53
taoye <1448499101@qq.com>
2018-04-28 13:53:44 +0800

    go 增加web demo

--- Go/hello/src/main/HelloWorld.go
+++ Go/hello/src/main/HelloWorld.go
@@ -1,9 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"log"
+	"io"
+)
+
 
 func main() {
-
+	
 	fmt.Print("Hello go")
 
+	http.HandleFunc("/",sayHello)
+
+	err := http.ListenAndServe(":8080",nil)
+	if err!=nil{
+		log.Fatal(err)
+	}
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request){
+	io.WriteString(w,"hello web")
 }
