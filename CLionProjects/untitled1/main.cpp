1651058a6c465b65314aa6cd51180b51f0e4b376
taoye <1448499101@qq.com>
2019-03-06 10:07:43 +0800

    2

--- CLionProjects/untitled1/main.cpp
+++ CLionProjects/untitled1/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include "windows.h"
 
+using namespace std;
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     MessageBox(NULL, "hello", "caption", MB_OK);
+#ifdef WIN32
+    cout << "i am windows" << std::endl;
+#else
+    cout << "i am unix" << std::endl;
+#endif
     return 0;
 }
